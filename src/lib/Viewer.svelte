a6d336103ff295448fc82aa29a11b8291d77cbcd
Angelos <evangelosas@yahoo.com>
2021-09-14 19:32:17 +0300

    Model loaded and play first animation

--- src/lib/Viewer.svelte
+++ src/lib/Viewer.svelte
@@ -4,43 +4,62 @@
     PerspectiveCamera,
     WebGLRenderer,
     DirectionalLight,
+    AnimationMixer,
+    Color,
+    Clock,
   } from "three";
   import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-  const scene = new Scene();
-  const camera = new PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 100);
-  camera.position.z = 0.5;
-  const loader = new GLTFLoader();
-  const light = new DirectionalLight(0xffffff, 1);
-  light.position.set(0, 0, 1);
+  let clock = new Clock();
+  let renderer, scene, camera, mixer;
 
-  loader.load(
-    "./Megan.glb",
-    function (gltf) {
-      console.log("Model was loaded successfully.");
-      console.log(gltf);
-      scene.add(gltf.scene);
-      animate();
+  init();
+  render();
 
-    },
-    function (xhr) {
-      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-    },
-    function (error) {
-      console.log("There was an error when tried to load the model.");
+  function init() {
+    scene = new Scene();
+    scene.background = new Color(0xf1f1f1);
+
+    camera = new PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 100);
+    camera.position.z = 0.5;
+
+    const light = new DirectionalLight(0xffffff, 1);
+    light.position.set(0, 0, 1);
+    scene.add(light);
+
+    const loader = new GLTFLoader();
+    loader.load(
+      "./Megan.glb",
+      function (gltf) {
+        console.log("Model was loaded successfully.");
+        console.log(gltf);
+        scene.add(gltf.scene);
+        playAnimations(gltf.scene, gltf.animations);
+      },
+      function (workload) {
+        console.log((workload.loaded / workload.total) * 100 + "% loaded");
+      },
+      function () {
+        console.log("There was an error when tried to load the model.");
+      }
+    );
+
+    function playAnimations(model, animations) {
+      mixer = new AnimationMixer(model);
+      const clip = animations[0];
+      const action = mixer.clipAction(clip);
+      action.play();
     }
-  );
-  
-  scene.add(light);
-  const renderer = new WebGLRenderer({antialias:true});
-  renderer.setSize(innerWidth, innerHeight);
-  document.body.appendChild(renderer.domElement);
-  
-  function animate() {
-    requestAnimationFrame(animate);
+
+    renderer = new WebGLRenderer({ antialias: true });
+    renderer.setSize(innerWidth, innerHeight);
+    document.body.appendChild(renderer.domElement);
+  }
+
+  function render() {
+    const dt = clock.getDelta();
+    if (mixer) mixer.update(dt);
+    requestAnimationFrame(render);
     renderer.render(scene, camera);
   }
-  
-
-
 </script>
