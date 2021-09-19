f5f8b59492081857239589e3ed865e706d5a192a
Angelos <evangelosas@yahoo.com>
2021-09-19 20:52:43 +0300

    Reformatting.

--- src/lib/EventRecorder.svelte
+++ src/lib/EventRecorder.svelte
@@ -1,25 +1,26 @@
-<script  lang="ts">
-import { createEventDispatcher } from "svelte";
-import { numberOfAnimations } from "./AnimationStore.js";
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import { numberOfAnimations } from "./AnimationStore.js";
 
-const dispatch = createEventDispatcher();
-export let url : string;
-const socket = new WebSocket(url);
-const START_PLAYING : boolean = true;
+  const dispatch = createEventDispatcher();
+  export let url: string;
+  const socket = new WebSocket(url);
+  const START_PLAYING: boolean = true;
 
-socket.addEventListener('message', function (event) {
-  let value = event.data;
-  console.log('Message from server: '+ value +" on " + new Date().toString() );
-  if ($numberOfAnimations > 0) {
-    dispatch("updateAnimation", {
-      index: convertValueToIndex(value),
-      status: START_PLAYING,
-    });
+  socket.addEventListener("message", function (event) {
+    let value = event.data;
+    console.log(
+      "Message from server: " + value + " on " + new Date().toString()
+    );
+    if ($numberOfAnimations > 0) {
+      dispatch("updateAnimation", {
+        index: convertValueToIndex(value),
+        status: START_PLAYING,
+      });
+    }
+  });
+
+  function convertValueToIndex(value: number): number {
+    return Math.floor(value * $numberOfAnimations) + 1;
   }
-});
-
-function convertValueToIndex(value : number) : number {
-  return Math.floor(value * $numberOfAnimations) + 1;
-}
-
 </script>
